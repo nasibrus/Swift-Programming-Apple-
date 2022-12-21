1abedf460f2fd7cf64d91c6f693be9188d3ad157
Ruslan Nasibov <44650808+nasibrus@users.noreply.github.com>
2022-12-21 03:25:43 +0100

    Push

---
+++ SimpleDiceApp-master/Dicee/DiceeTests/DiceeTests.swift
@@ -0,0 +1,33 @@
+//
+//  DiceeTests.swift
+//  DiceeTests
+//
+//  Dicee IOS13.5
+//  Created by Ruslan Nasibov
+
+import XCTest
+@testable import Dicee
+
+class DiceeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ SimpleDiceApp-master/README.md
@@ -0,0 +1,11 @@
+# SimpleDiceApp
+It is my first project in IOS development.This app has a very easy algorithm for creating.After designing just using arrays for changing dice.
+Here is photo description:
+
+
+<img width="328" alt="Screen Shot 2020-07-10 at 4 19 08 PM" src="https://user-images.githubusercontent.com/44650808/87164696-5e07b580-c2c9-11ea-96b7-9d50834f0829.png">
+
+<img width="328" alt="Screen Shot 2020-07-10 at 4 20 06 PM" src="https://user-images.githubusercontent.com/44650808/87164681-59db9800-c2c9-11ea-85d3-874f4af61ac5.png">
+
+Here is my algorithm:
+![image](https://user-images.githubusercontent.com/44650808/87164923-af17a980-c2c9-11ea-9310-cd043a3f2cda.png)
